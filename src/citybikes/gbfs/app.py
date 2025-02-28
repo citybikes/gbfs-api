b7bbc69f82b13c263e264ed720f5ae9f11e37983
eskerda <eskerda@gmail.com>
2025-02-28 21:44:54 +0100

    add the simplest home

--- src/citybikes/gbfs/app.py
+++ src/citybikes/gbfs/app.py
@@ -1,12 +1,16 @@
 import contextlib
 import os
 
+
 from starlette.applications import Starlette
-from starlette.routing import Mount
+from starlette.routing import Mount, Route
+from starlette.responses import Response
+
 
 from citybikes.db.asyncio import CBD, get_session, migrate
 from citybikes.gbfs.versions.v3.api import Gbfs as Gbfs3
 from citybikes.gbfs.versions.v2.api import Gbfs as Gbfs2
+from citybikes.gbfs.pages import HOME
 
 
 DB_URI = os.getenv("DB_URI", "citybikes.db")
@@ -28,9 +32,12 @@ async def lifespan(app):
 gbfs_v2 = Gbfs2()
 gbfs_v3 = Gbfs3()
 
+
 routes = [
     Mount("/2", routes=gbfs_v2.routes),
     Mount("/3", routes=gbfs_v3.routes),
+    # XXX this will do for now
+    Route("/", lambda r: Response(HOME.format(endpoint=str(r.base_url)))),
 ]
 
 app = Starlette(
---
+++ src/citybikes/gbfs/pages.py
@@ -0,0 +1,31 @@
+HOME = """Citybikes GBFS API
+------------------
+
+Welcome to the Citybikes GBFS API.
+
+This endpoint provides real-time bike share information in GBFS format.
+
+Urls look like:
+
+GET {endpoint}<version>/<network>/<feed>.json
+
+For example:
+
+GET {endpoint}2/bicing/gbfs.json
+GET {endpoint}2/bicing/system_information.json
+
+GET {endpoint}3/manifest.json
+GET {endpoint}3/bicing/gbfs.json
+GET {endpoint}3/bicing/system_information.json
+
+
+See the full list of networks available at https://citybik.es
+
+Enjoy!
+
+----
+
+GBFS: https://gbfs.org
+Docs: https://docs.citybik.es/api/gbfs
+Code: https://github.com/citybikes/gbfs-api
+"""
